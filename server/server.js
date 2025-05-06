0811f84b6a95c713c4296f21bd2cb39f3a4d2cdd
alameenshareef <ameenshareef03@gmail.com>
2025-05-06 21:18:33 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,12 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
+
+app.get('/',(req,res)=>{
+
+  res.json("welcome")
+})
+
 if (process.env.NODE_ENV !== 'test') {
   mongoose.connect(MONGO_URI)
     .then(() => {
